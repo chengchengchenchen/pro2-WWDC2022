e142a83b39541867c7f13855089f5127fbd213ce
chengchengchenchen <772809505@qq.com>
2023-06-11 22:17:01 +0800

    Initial Commit

---
+++ WWDC2022-Grid/WWDC2022_GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  WWDC2022_GridApp.swift
+//  WWDC2022-Grid
+//
+//  Created by qjc on 2023/6/11.
+//
+
+import SwiftUI
+
+@main
+struct WWDC2022_GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WWDC2022-GridUITests/WWDC2022_GridUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WWDC2022_GridUITestsLaunchTests.swift
+//  WWDC2022-GridUITests
+//
+//  Created by qjc on 2023/6/11.
+//
+
+import XCTest
+
+final class WWDC2022_GridUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
